27daa97a7936bd8b2932d626c395132f32d259b0
codytheking <codytheking93@gmail.com>
2018-08-20 14:00:47 -0700

    clarification for output

---
+++ Arithmetic.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Arithmetic here.
+ *
+ * @author King
+ * @version 8/20
+ */
+public class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        int length = 5;
+        int width = 7;
+        int area = length * width;
+        double base1 = 2.5;
+        double base2 = 3.25;
+        double height = 10.1;
+        double trapArea = (base1 + base2) * height / 2;
+        System.out.println("Trapezoid area: " + trapArea);
+        System.out.println("Length: " + length);
+        System.out.println("Width: " + width);
+        System.out.println("Area = " + area);
+        System.out.println("4 + 5 * 8 = " + (4 + 5 * 8));
+        System.out.println((4 + 5) * 8);
+    }
+}
+
+
+
